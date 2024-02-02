3a990f3a408f5c11f560b9e520537a05d7a962fb
Michal Paleczny <paleczny.michal@gmail.com>
2024-02-02 18:20:24 +0000

    added basic function and questions for readme

--- index.js
+++ index.js
@@ -1,21 +1,93 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+import fs from 'fs/promises';
+import inquirer from 'inquirer';
 
-// array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
+async function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is your GitHub username? (No @ needed)",
+            name: 'username',
+            default: 'connietran-dev',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub username is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the name of your GitHub repo?",
+            name: 'repo',
+            default: 'readme-generator',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid GitHub repo is required for a badge.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "What is the title of your project?",
+            name: 'title',
+            default: 'Project Title',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid project title is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "Write a description of your project.",
+            name: 'description',
+            default: 'Project Description',
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid project description is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            message: "If applicable, describe the steps required to install your project for the Installation section.",
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: "Provide instructions and examples of your project in use for the Usage section.",
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+            name: 'contributing'
+        },
+        {
+            type: 'input',
+            message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+            name: 'tests'
+        },
+        {
+            type: 'list',
+            message: "Choose a license for your project.",
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+            name: 'license'
+        }
+    ]);
 }
 
-// function to initialize program
-function init() {
-
+async function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data);
 }
+
+promptUser()
+    .then(answers => generateHTML(answers))
+    .then(html => writeToFile('portfolio.html', html))
+    .catch(error => console.error('There was an error:', error));
 
 // function call to initialize program
 init();
